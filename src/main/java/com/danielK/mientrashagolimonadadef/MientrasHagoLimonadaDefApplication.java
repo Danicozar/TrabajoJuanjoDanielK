79f198bd20ace40e10f8f355121697d8555c4c1b
Dani <danikuperden@gmail.com>
2023-02-13 16:54:18 +0100

    Initial commit

---
+++ src/main/java/com/danielK/mientrashagolimonadadef/MientrasHagoLimonadaDefApplication.java
@@ -0,0 +1,13 @@
+package com.danielK.mientrashagolimonadadef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MientrasHagoLimonadaDefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MientrasHagoLimonadaDefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danielK/mientrashagolimonadadef/MientrasHagoLimonadaDefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielK.mientrashagolimonadadef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MientrasHagoLimonadaDefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
